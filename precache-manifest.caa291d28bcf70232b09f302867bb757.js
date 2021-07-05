79f6274138f677728df58a8822a900adaa35bf96
anshulchouhan2209 <chouhananshul2209@gmail.com>
2021-07-05 16:35:39 +0530

    Updates

---
+++ precache-manifest.caa291d28bcf70232b09f302867bb757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b0c6e9ffbb950b57e275a1cb1aa1c0f",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "d65abb2dde141b6dc80b",
+    "url": "/bmi-calculator/static/css/main.fce36487.chunk.css"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/js/2.0e5f39ad.chunk.js"
+  },
+  {
+    "revision": "d65abb2dde141b6dc80b",
+    "url": "/bmi-calculator/static/js/main.5bf1b6cf.chunk.js"
+  },
+  {
+    "revision": "9b25b4e690523b173542",
+    "url": "/bmi-calculator/static/js/runtime~main.425d477f.js"
+  }
+]);
